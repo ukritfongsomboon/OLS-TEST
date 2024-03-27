67df81f837626e482892391c5464a11c5a68709d
Ukrit Fongsomboon <80668919+ukritfongsomboon@users.noreply.github.com>
2024-03-27 23:10:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/api/v1/test', methods=['GET'])
 def test():
-    flask_version = Flask.__version__
-    return jsonify({"flask_version": flask_version}), 200
+    # flask_version = Flask.__version__
+    # return jsonify({"flask_version": flask_version}), 200
+    return "ok"
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
